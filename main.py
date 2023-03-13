f19c27bac510880b7317d42c99eb45ac54c6339c
Nicob04 <107031363+Nicob04@users.noreply.github.com>
2023-03-13 14:43:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #GUESS THE NUMBER GAME IN PYTHON
 from genera import rand
-import random 
+
 def main():
-    print('ciao')
     print('Rule:\nGuess the number with wax 10 attemps')
-    print('The rage in 0-100')
+    print('The rage in 0-200')
     print('GOOD LUCK')
     i = 0
     trovato = False
     n = rand()
-    #print(n)
     while(i < 10):
         print('guess the number:')
         ans = int(input())
@@ -31,7 +29,7 @@ def main():
     else:    
         print('you do not guess the rigth number sorry \nThe ritgh number was', n)
 
-    
+
 #richiamo main
 if __name__ == "__main__":
     main()
